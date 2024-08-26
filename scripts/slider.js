334fae9b53d4249eb670b6d973399f4170ac7205
Westr <si.studios13@gmail.com>
2024-08-26 15:26:41 -0700

    Added more content
    about and sevices
    responsivity in progress

---
+++ scripts/slider.js
@@ -0,0 +1,15 @@
+function serviceSlider() {
+    return {
+        currentSlide: 0,
+        totalSlides: 4, // Total number of services
+        init() {
+            // Optional: Auto-slide functionality
+            
+        },
+        autoSlide() {
+            setInterval(() => {
+                this.currentSlide = (this.currentSlide + 1) % this.totalSlides;
+            }, 5000); // Slide every 5 seconds
+        }
+    };
+}
